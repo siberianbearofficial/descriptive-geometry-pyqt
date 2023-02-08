eba7459bfd3b1d984d816983fee06bf1c257e09a
siberianbearofficial <alexeyorlov65@gmail.com>
2023-02-08 21:14:34 +0300

    Обновлена сериализация/десериализация. На данный момент работает все, кроме сплайнов (и содержащих их объектов).

--- utils/history/serializable.py
+++ utils/history/serializable.py
@@ -1,11 +1,10 @@
 import utils.maths.angem as ag
 
-
 angem_objects = {
     ag.Point: ['x', 'y', 'z'],
     ag.Vector: ['x', 'y', 'z'],
     ag.Line: ['point', 'vector'],
-    ag.Plane: ['point', 'normal'],
+    ag.Plane: ['normal', 'point'],
     ag.Segment: ['p1', 'p2'],
     ag.Circle: ['center', 'radius', 'normal'],
     ag.Arc: ['p1', 'p2', 'center', 'big_arc'],
@@ -18,3 +17,5 @@ angem_objects = {
     ag.Spline3D: ['points'],
     ag.RotationSurface: ['center1', 'center2', 'spline1'],
 }
+
+angem_class_by_name = {key.__name__: key for key in angem_objects}
--- utils/history/serializer.py
+++ utils/history/serializer.py
@@ -1,5 +1,4 @@
 import json
-import utils.maths.angem as ag
 
 from utils.drawing.screen import Screen
 from utils.drawing.layer import Layer
@@ -8,12 +7,16 @@ from utils.drawing.general_object import GeneralObject
 from utils.drawing.command_line import CommandLine
 from utils.drawing.toolbar import Toolbar2
 
+from utils.history.serializable import angem_objects, angem_class_by_name
 
-def serialize(screen):
+
+def serialize(screen, path='history.txt'):
     """
     Function to serialize screen history to file.
     :param screen: screen to serialize
+    :param path: path to the history file
     """
+
     class Serializer(json.JSONEncoder):
         def default(self, obj):
             if isinstance(obj, Plot) or isinstance(obj, Layer) or isinstance(obj, Screen):
@@ -25,20 +28,32 @@ def serialize(screen):
                 return {'ag_object': obj.ag_object, 'color': obj.color}
             elif isinstance(obj, CommandLine) or isinstance(obj, Toolbar2):
                 return ''
+            return try_serialize(obj)
 
-            try:
-                return {'__{}__'.format(obj.__class__.__name__): obj.__dict__}
-            except AttributeError:
-                return str(obj)
+    def try_serialize(obj):
+        for key in angem_objects:
+            if isinstance(obj, key):
+                return serialize_object(obj)
+        try:
+            return {'__{}__'.format(obj.__class__.__name__): obj.__dict__}
+        except AttributeError:
+            return str(obj)
 
-    hist = json.dumps(screen, cls=Serializer, indent=2)
-    print(hist, file=open('history.txt', 'w', encoding='utf-8'), end='')
+    def serialize_object(obj):
+        to_serialize = {'name': obj.__class__.__name__}
+        for field_name in angem_objects[obj.__class__]:
+            to_serialize[field_name] = obj.__dict__.get(field_name, '')
+        return to_serialize
+
+    hist = json.dumps(screen, cls=Serializer, indent=2)  # TODO: remove indent to save disk space
+    print(hist, file=open(path, 'w', encoding='utf-8'), end='')
 
 
-def deserialize(screen):
+def deserialize(screen, path='history.txt'):
     """
     Function to deserialize history from file and apply it to the screen.
     :param screen: screen to draw on
+    :param path: path to the history file
     """
 
     def deserialize_layers(layers_hist):
@@ -46,64 +61,42 @@ def deserialize(screen):
         for layer_hist in layers_hist:
             layer = Layer(screen.plot, layer_hist['name'], layer_hist['hidden'])
             for obj_hist in layer_hist['objects']:
-                obj = deserialize_ag_object(obj_hist['ag_object'])
+                obj = try_deserialize_ag_object(obj_hist['ag_object'])
                 layer.add_object(obj, obj_hist['color'])
             layers.append(layer)
         return layers
 
-    def deserialize_ag_object(hist):
-        if '__Point__' in hist:
-            ag_hist = hist['__Point__']
-            return ag.Point(ag_hist['x'], ag_hist['y'], ag_hist['z'])
-        elif '__Segment__' in hist:
-            ag_hist = hist['__Segment__']
-            return ag.Segment(deserialize_ag_object(ag_hist['p1']),
-                              deserialize_ag_object(ag_hist['p2']))
-        elif '__Circle__' in hist:
-            ag_hist = hist['__Circle__']
-            return ag.Circle(deserialize_ag_object(ag_hist['center']), ag_hist['radius'])
-        elif '__Line__' in hist:
-            ag_hist = hist['__Line__']
-            return ag.Line(deserialize_ag_object(ag_hist['point']),
-                           deserialize_ag_object(ag_hist['vector']))
-        elif '__Vector__' in hist:
-            ag_hist = hist['__Vector__']
-            return ag.Vector(ag_hist['x'], ag_hist['y'], ag_hist['z'])
-        elif '__Cylinder__' in hist:
-            ag_hist = hist['__Cylinder__']
-            return ag.Cylinder(deserialize_ag_object(ag_hist['center1']),
-                               deserialize_ag_object(ag_hist['center2']),
-                               ag_hist['radius'], deserialize_ag_object(ag_hist['vector']))
-        elif '__Cone__' in hist:
-            ag_hist = hist['__Cone__']
-            return ag.Cone(deserialize_ag_object(ag_hist['center1']),
-                           deserialize_ag_object(ag_hist['center2']),
-                           ag_hist['radius1'],
-                           ag_hist['radius2'],
-                           deserialize_ag_object(ag_hist['vector']))
-        elif '__Tor__' in hist:
-            ag_hist = hist['__Tor__']
-            return ag.Tor(deserialize_ag_object(ag_hist['center']),
-                          ag_hist['radius'],
-                          ag_hist['tube_radius'],
-                          deserialize_ag_object(ag_hist['vector']))
-        elif '__RotationSurface__' in hist:
-            ag_hist = hist['__RotationSurface__']
-            return ag.RotationSurface(deserialize_ag_object(ag_hist['center1']),
-                                      deserialize_ag_object(ag_hist['center2']),
-                                      deserialize_ag_object(ag_hist['spline1']))
-        elif '__Spline__' in hist:
-            ag_hist = hist['__Spline__']
-            return ag.Spline(deserialize_ag_object(ag_hist['plane']),
-                             )
-        elif '__Plane__' in hist:
-            ag_hist = hist['__Plane__']
-            return ag.Plane(deserialize_ag_object(ag_hist['normal']),
-                            deserialize_ag_object(ag_hist['point']))
-        else:
-            raise ValueError('Unknown ag_object type: ' + str(hist))
+    def try_deserialize_ag_object(serialized):
+        if isinstance(serialized, dict) and 'name' not in serialized:
+            return serialized.values()
+        elif not isinstance(serialized, dict):
+            return serialized
+        object_class = angem_class_by_name.get(serialized['name'], None)
+        if object_class:
+            params = list()
+            for key, val in serialized.items():
+                if key != 'name':
+                    params.append(try_deserialize_ag_object(val))
 
-    hist = json.loads(open('history.txt', encoding='utf-8').read())
+            show_exception = object_instance = None
+            try:
+                object_instance = object_class(*params)
+            except ValueError as e:
+                print('Serialized:', serialized)
+                print('Params for instantiating last object:', *params)
+                show_exception = e
+            if show_exception is not None:
+                raise AttributeError(
+                    f'Exception occurred while instantiating object: {show_exception}.\nPossible reason: inaccurate '
+                    f'class specification for {object_class.__name__}.\nCheck: {angem_objects[object_class]}.\n'
+                    f'Also check the given file not to be broken or outdated.'
+                )
+            else:
+                return object_instance
+        else:
+            raise AttributeError('Angem object undefined:', serialized['name'])
+
+    hist = json.loads(open(path, encoding='utf-8').read())
     screen.update(hist['title'], hist['bg_color'])
     plot_hist = hist['plot']
     screen.plot.update(plot_hist['bg_color'], deserialize_layers(plot_hist['layers']))
